b1a720ef47c92813d96cbcbdd20c1fdf30c1a033
Laurine Denis <ldenis@z2r12p6.42lyon.fr>
2022-10-04 20:36:22 +0200

    kick OK ban to remove

--- src/CommandChannel.cpp
+++ src/CommandChannel.cpp
@@ -3,7 +3,7 @@
 void	ExecutionManager::change_topic(std::string topic, std::string user, Channel *channel)
 {
 	// char			time_test[300];
-	time_t timestamp = time( NULL );
+	time_t	timestamp = time( NULL );
     // struct tm * pTime = localtime( & timestamp );
 	// strftime(time_test, 300, "%a, %d %h %G %T", pTime);
 	// std::string		time_string;
@@ -22,7 +22,7 @@ void	ExecutionManager::command_topic(std::vector<std::string> out, User *user)
 	{
 		channel_name = out[1].erase(0, 1);
 		if ((channel = find_channel(channel_name)) == NULL)
-			user->answer = out[1] + " No such channel" + ENDLINE;
+			user->answer = ":server 403 " + out[1] + " No such channel" + ENDLINE;
 		else
 		{
 			std::cout << "Topic Time == " << channel->get_topic_time() << std::endl;
@@ -34,7 +34,7 @@ void	ExecutionManager::command_topic(std::vector<std::string> out, User *user)
 					user->answer = ":server 332 " +  user->get_name() + " #" + channel->get_name() + " :" + channel->get_topic() + ENDLINE + ":server 333 " +  user->get_name() + " #" + channel->get_name() + " " + channel->get_topic_user() + " " + channel->get_topic_time() + ENDLINE;
 			}
 			else if (out[2][0] != ':')
-				user->answer = "ERR_NEEDMOREPARAMS";
+				user->answer = ":server 461 " +  user->get_name() + " TOPIC :Not enough parameters" + ENDLINE;
 			else
 			{
 				change_topic(out[2].erase(0, 1), user->get_name(), channel);
@@ -44,7 +44,7 @@ void	ExecutionManager::command_topic(std::vector<std::string> out, User *user)
 	}
 	else
 	{
-		user->answer = out[1] + " No such channel" + ENDLINE;
+		user->answer = ":server 403 " + out[1] + " No such channel" + ENDLINE;
 	}
 }
 
@@ -55,26 +55,29 @@ void	ExecutionManager::command_join(std::vector<std::string> out, User *user, st
 	Channel						*channel;
 	std::string					channel_name;
 	std::vector<std::string>	split_comma;
-	int							i = 1;
+	int							i = 0;
 	std::string					msg = "";
 
 	// std::cout << out.size() << std::endl;
 	str.erase(0,5);
 	split_comma = split_vector(str, "\r\n,");
+	for (std::vector<std::string>::iterator it = split_comma.begin(); it != split_comma.end(); ++it)
+		std::cout << "|" << *it << "|" << std::endl;
 	while (i < split_comma.size())
 	{
+		std::cout << "channel join = " << split_comma[i] << std::endl;
 		if (split_comma[i][0] != '#')
 		{
 			std::cout << "# OU PAS " << split_comma[i][0] << std::endl;
-			user->answer += split_comma[i] + " No such channel" + ENDLINE;
+			user->answer += ":server 403 " + split_comma[i] + " No such channel" + ENDLINE;			
 			i++;
 		}
 		else
 		{
 			//sortir boucle si espace pas voulu
+			std::cout << "channel join = " << split_comma[i] << std::endl;
 			if (split_comma[i - 1][split_comma[i - 1].size() - 1] == ' ')
 				break;
-			std::cout << "channel join = " << split_comma[i] << std::endl;
 			channel_name = split_comma[i].erase(0, 1);
 			if ((channel = find_channel(channel_name)) == NULL)
 			{
@@ -84,7 +87,11 @@ void	ExecutionManager::command_join(std::vector<std::string> out, User *user, st
 				std::cout << "push back name = " << channel->get_name() << std::endl;
 				_channels->push_back(*channel);
 			}
-			add_channel_in_user(channel, user);
+			if (add_channel_in_user(channel, user) == false)
+			{
+				user->answer = ":server 474 " + user->get_nickname() + " " + channel->get_name() + " :Cannot join channel (+b)" + ENDLINE;
+				return ;
+			}
 			user->answer += ":" + user->get_nickname() + "!" + user->get_nickname() + "@server JOIN #" + channel->get_name() + ENDLINE;
 			if (channel->get_topic() != "")
 				user->answer += ":server 332 " +  user->get_name() + " #" + channel->get_name() + " :" + channel->get_topic() + ENDLINE + ":server 333 " +  user->get_name() + " #" + channel->get_name() + " " + channel->get_topic_user() + " " + channel->get_topic_time() + ENDLINE;
@@ -106,29 +113,28 @@ void	ExecutionManager::command_join(std::vector<std::string> out, User *user, st
 	}
 	if (i != split_comma.size())
 	{
-		user->answer = out[i - 1] + " No such channel" + ENDLINE;
+		user->answer = ":server 403 " + out[i - 1] + " No such channel" + ENDLINE;
 	}
 }
 
 bool	is_in_channel(Channel *channel, User *user)
 {
-	int i = 0;
-	for (std::vector<User>::iterator ite = channel->_users->begin(); ite != channel->_users->end(); ite++, i++)
+	for (std::vector<User>::iterator ite = channel->_users->begin(); ite != channel->_users->end(); ite++)
 		if (ite->get_nickname() == user->get_nickname())
 			return (true) ;
 	return (false);
 }
 
-void	add_channel_in_user(Channel *channel, User *user)
+bool	add_channel_in_user(Channel *channel, User *user)
 {
 	//add channel in user
-	std::cout << "Add channel in user" << std::endl;
-	std::cout << "User : " << user->get_nickname() << std::endl;
-	std::cout << "Channel : " << channel->get_name() << std::endl;
+	if (channel->is_banned(user) == true)
+		return (false);
 	user->_channels->push_back(*channel);
 	user->set_nb_channel(user->get_nb_channel() + 1);
 	if (is_in_channel(channel, user) == false)
 		channel->_users->push_back(*user);
+	return (true);
 }
 
 void	ExecutionManager::delete_channel(Channel *channel)
@@ -175,7 +181,7 @@ void	ExecutionManager::command_part(std::vector<std::string> out, User *user)
 		{
 			//Le channel n'existe pas => Pas de message d'erreurs ?
 			std::cout << "No Channel found" << std::endl;
-			user->answer = out[1] + " No such channel" + ENDLINE;
+			user->answer = ":server 403 " + out[1] + " No such channel" + ENDLINE;
 		}
 		else
 		{
@@ -188,15 +194,21 @@ void	ExecutionManager::command_part(std::vector<std::string> out, User *user)
 	}
 	else
 	{
-		user->answer = out[1] + " No such channel" + ENDLINE;
+		user->answer = ":server 403 " + out[1] + " No such channel" + ENDLINE;
 	}
+}
+
+bool	ExecutionManager::is_operator(User *user, Channel *channel)
+{
+	if (user->get_nickname() == channel->get_operator()->get_nickname())
+		return (true);
+	return (false);
 }
 
 void	ExecutionManager::command_kick(std::vector<std::string> out, User *user)
 {
 	std::cout << "command Kick" << std::endl;
 	Channel		*channel;
-	
 	std::string	channel_name;
 
 	if (out[1][0] == '#')
@@ -206,16 +218,31 @@ void	ExecutionManager::command_kick(std::vector<std::string> out, User *user)
 		{
 			//Le channel n'existe pas => Pas de message d'erreurs ?
 			std::cout << "No Channel found" << std::endl;
-			user->answer = out[1] + " No such channel" + ENDLINE;
+			user->answer = ":server 403 " + out[1] + " No such channel" + ENDLINE;
 		}
 		else
 		{
-			
-			user->answer = ":" + user->get_nickname() + "!" + user->get_name() + "@server PART #" + channel->get_name() + ENDLINE;
+			if (is_operator(user, channel) == true)
+			{
+				User	*userToBan;
+				userToBan = find_user(out[2]);
+				// remove user from channel
+				remove_user_of_channel(channel, userToBan);
+				// add user in banned list
+				channel->banned_user(userToBan);
+				// send msg to user banned
+				user->answer = ":" + user->get_nickname() + "!" + user->get_name() + "@server KICK #" + channel->get_name() + " " + out[2] + ENDLINE;
+				// send msg to user who banned
+				send_msg_to_user(user->answer, userToBan);
+			}
+			else
+			{
+				user->answer = ":server 482 " + user->get_nickname() + " #" + out[1] + " :You're not channel operator" + ENDLINE;
+			}
 		}
 	}
 	else
 	{
-		user->answer = out[1] + " No such channel" + ENDLINE;
+		user->answer = ":server 403 " + out[1] + " No such channel" + ENDLINE;
 	}
 }
